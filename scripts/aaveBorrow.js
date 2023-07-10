dfd6a0fec32fcfc8a66abe999b6d2dc4df3586b2
thomasvloo <vanloo.thomas@yahoo.com>
2023-07-10 14:12:25 +0200

    add function to get aave lending pool and WETH

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,4 +1,32 @@
-async function main() {}
+const { getNamedAccounts, ethers } = require("hardhat")
+const { getWeth } = require("../scripts/getWeth")
+
+async function main() {
+    await getWeth()
+    const { deployer } = await getNamedAccounts()
+    // Lending Pool Address provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+    // lending pool: ^
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`Lending pool address ${lendingPool.address}`)
+    // deposit
+    const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    // approve
+}
+
+async function getLendingPool(account) {
+    const lendingPoolAddressesProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+        account
+    )
+    const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, account)
+    return lendingPool
+}
+
+async function approveErc20(contractAddress, spenderAddress, amountToSpend, account) {
+    const erc20Token = await ethers.getContractAt("")
+}
 
 main()
     .then(() => process.exit(0))
